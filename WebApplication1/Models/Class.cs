b1344887426a4b8774fae08bfc230b128f8796ef
Patryk Rychły <sellstorm1@gmail.com>
2024-01-21 21:48:20 +0100

    i created a model with required

---
+++ WebApplication1/Models/Class.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Class
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public string Category { get; set; }
+
+        public DateTime DateOfCreate { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+    }
+}
